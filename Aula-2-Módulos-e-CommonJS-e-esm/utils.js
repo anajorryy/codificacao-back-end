694c0dcfb77ba6ce4545eb7a159aba65231785c8
Mariana_Jorry <marianajorry.1@gmail.com>
2026-09-23 15:13:33 -0300

    feat: implement log organizer with ES modules

---
+++ Aula-2-Módulos-e-CommonJS-e-esm/utils.js
@@ -0,0 +1,8 @@
+// utils.js
+
+export function formatLog(mensagem) {
+    const dataAtual = new Date().toISOString().split('T')[0];
+    const horaAtual = new Date().toLocaleTimeString();
+
+    return `[${dataAtual} ${horaAtual}] - ${mensagem}\n`;
+}
